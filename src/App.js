cefd256a58bca42916b2f1b2b803397d472b1aa1
Vova Yankers <vovarojko@gmail.com>
2023-01-29 22:02:00 +0200

    added fucn search and choose category (only active)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React, { useEffect, useState } from 'react'
 import Collection from './Collection'
 import './index.scss'
 
+const cats = [
+  { name: 'Все' },
+  { name: 'Море' },
+  { name: 'Горы' },
+  { name: 'Архитектура' },
+  { name: 'Города' },
+]
+
 function App() {
+  const [categoryId, setCategortId] = useState(0) // All category - 0
+  const [seacrhValue, setSearchValue] = useState('')
+  console.log(seacrhValue)
   const [collections, setCollections] = useState([])
   console.log(collections)
   useEffect(() => {
@@ -17,27 +28,37 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Моя коллекция фотографий</h1>
+      <h1 style={{ color: 'black' }}>Моя коллекция фотографий</h1>
       <div className="top">
         <ul className="tags">
-          <li className="active">Все</li>
-          <li>Горы</li>
-          <li>Море</li>
-          <li>Архитектура</li>
-          <li>Города</li>
+          {/* <li className="active">Все</li> */}
+          {cats.map((obj, i) => (
+            <li
+              onClick={() => setCategortId(i)}
+              className={categoryId === i ? 'active' : ''}
+              key={obj.name}
+            >
+              {obj.name}
+            </li>
+          ))}
         </ul>
-        <input className="search-input" placeholder="Поиск по названию" />
+        <input
+          value={seacrhValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          className="search-input"
+          placeholder="Поиск по названию"
+        />
       </div>
       <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
-        />
+        {collections
+          .filter((obj) => {
+            if (obj.name.toLowerCase().includes(seacrhValue.toLowerCase())) {
+              return obj.name
+            }
+          })
+          .map((obj, index) => (
+            <Collection key={index} name={obj.name} images={obj.photos} />
+          ))}
       </div>
       <ul className="pagination">
         <li>1</li>
